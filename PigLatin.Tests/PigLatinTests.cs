f8df7066958528be0f71d38b8e61c6a51a701a79
Eddy <eddyharrington@gmail.com>
2018-06-17 14:20:36 -0700

    Single word consonant conversion added
    Single consonant words convert to pig latin. Tests included.

--- PigLatin.Tests/PigLatinTests.cs
+++ PigLatin.Tests/PigLatinTests.cs
@@ -13,10 +13,62 @@ namespace PigLatin.Tests
             string input = "banana";
 
             //Act
-            string output = PigLatin.GetValue(input);
+            string output = PigLatin.ConvertToPigLatin(input);
 
             //Assert
             Assert.AreEqual("ananabay", output);
+        }
+
+        [TestMethod]
+        public void PigLatin_WhenPig_ReturnsIgpay()
+        {
+            //Arrange
+            string input = "pig";
+
+            //Act
+            string output = PigLatin.ConvertToPigLatin(input);
+
+            //Assert
+            Assert.AreEqual("igpay", output);
+        }
+
+        [TestMethod]
+        public void PigLatin_WhenToo_ReturnsOotay()
+        {
+            //Arrange
+            string input = "Too";
+
+            //Act
+            string output = PigLatin.ConvertToPigLatin(input);
+
+            //Assert
+            Assert.AreEqual("ootay", output);
+        }
+
+        [TestMethod]
+        public void PigLatin_WhenSimple_ReturnsImplesay()
+        {
+            //Arrange
+            string input = "Simple";
+
+            //Act
+            string output = PigLatin.ConvertToPigLatin(input);
+
+            //Assert
+            Assert.AreEqual("implesay", output);
+        }
+
+        [TestMethod]
+        public void PigLatin_WhenPoo_ReturnsOopay()
+        {
+            //Arrange
+            string input = "pOo";
+
+            //Act
+            string output = PigLatin.ConvertToPigLatin(input);
+
+            //Assert
+            Assert.AreEqual("oopay", output);
         }
     }
 }
--- PigLatin/PigLatin.cs
+++ PigLatin/PigLatin.cs
@@ -7,11 +7,29 @@ using System.Windows.Forms;
 
 namespace PigLatin
 {
-    public class PigLatin
+    public static class PigLatin
     {
-        public static string GetValue(string input)
+        public static string[] Consonants = { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t",
+                "v", "w", "x", "y", "z" };
+        private static string ConsonantCluster = "group of consonants with no intervening vowel, " +
+            "e.g. 'splits'...spl AND ts are clusters, but the i is a vowel.";
+        private static string[] Vowels = { "a", "e", "i", "o", "u" };
+
+        public static string ConvertToPigLatin(string input)
         {
-            return "ananabay";
+            string output = string.Empty;
+            string firstLetterOfInput = input.Substring(0, 1).ToLower();
+
+            foreach (string s in Consonants)
+            {
+                if (firstLetterOfInput == s)
+                {
+                    string inputWithoutConsonant = input.Substring(1, input.Length - 1).ToLower();
+                    output = inputWithoutConsonant + firstLetterOfInput + "ay";
+                }
+            }
+
+            return output;
         }
     }
 }
